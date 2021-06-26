91f7c34d9980362ae0574d90ca49e089b642699a
p3tr0v <p3tr0v@protonmail.com>
2021-06-25 22:38:47 -0300

    1.0.1
    New programs in process list

--- utils/process_list.go
+++ utils/process_list.go
@@ -4,8 +4,11 @@ import (
 	"strings"
 )
 
-var ProcessListToCheck = []string{"processhacker.exe", "procmon.exe", "pestudio.exe", "procmon64.exe", "x32dbg.exe", "x64dbg.exe", "CFF Explorer.exe",
-	"procexp64.exe", "procexp.exe", "pslist.exe", "tcpview.exe", "tcpvcon.exe", "dbgview.exe", "wireshark.exe", "RAMMap.exe", "RAMMap64.exe", "vmmap.exe", "ollydbg.exe", "agent.py"}
+var ProcessListToCheck = []string{"processhacker.exe", "procmon.exe", "pestudio.exe", "procmon64.exe", "x32dbg.exe", "x64dbg.exe", "CFF Explorer.exe", "procexp64.exe",
+	"procexp.exe", "pslist.exe", "tcpview.exe", "tcpvcon.exe", "dbgview.exe", "RAMMap.exe", "RAMMap64.exe", "vmmap.exe", "ollydbg.exe", "agent.py", "autoruns.exe",
+	"autorunsc.exe", "filemon.exe", "regmon.exe", "idaq.exe", "idaq64.exe", "ImmunityDebugger.exe", "Wireshark.exe", "dumpcap.exe", "HookExplorer.exe", "ImportREC.exe", "PETools.exe",
+	"LordPE.exe", "SysInspector.exe", "proc_analyzer.exe", "sysAnalyzer.exe", "sniff_hit.exe", "windbg.exe", "joeboxcontrol.exe", "joeboxserver.exe", "joeboxserver.exe", "ResourceHacker.exe", "Fiddler.exe",
+	"httpdebugger.exe"}
 
 var MemoryDumpListToCheck = []string{"DumpIt.exe", "RAMMap.exe", "RAMMap64.exe", "vmmap.exe"}
 
